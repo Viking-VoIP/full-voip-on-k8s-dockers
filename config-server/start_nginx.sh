f1d92a717276e41ccd234c3910654dec6577c2ec
David Villasmil <david.villasmil.work@gmail.com>
2021-08-24 19:22:56 +0100

    Moving to the right github repo

--- config-server/start_nginx.sh
+++ config-server/start_nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rm -rf /var/www/html/
-git clone https://github.com/davidcsi/voip-full-k8s-config-server.git /var/www/html
+git clone https://github.com/Viking-VoIP/full-voip-on-k8s-config-server-config.git /var/www/html
 #nginx -g 'daemon off;'
 service nginx start
--- kamailio/start_kam.sh
+++ kamailio/start_kam.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf /etc/kamailio
-git clone https://github.com/davidcsi/voip-full-k8s-sip-proxy.git /etc/kamailio
+git clone https://github.com/Viking-VoIP/full-voip-on-k8s-sip-proxy-config.git /etc/kamailio
 cd /etc/kamailio/ && bash initialize.sh
