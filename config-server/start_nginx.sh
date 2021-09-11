b183a83ab80e0a321f0db37fa9ead0c3e1f8f374
David Villasmil <david.villasmil.work@gmail.com>
2021-09-11 23:35:18 +0100

    Several changes

--- config-server/start_nginx.sh
+++ config-server/start_nginx.sh
@@ -2,5 +2,5 @@
 
 rm -rf /var/www/html/
 git clone https://github.com/Viking-VoIP/full-voip-on-k8s-config-server-config.git /var/www/html
-#nginx -g 'daemon off;'
+bash /var/www/html/init.sh
 service nginx start
