1fd4cab43db54067a50d5e6ccfd09c56b647b48a
David Villasmil <david.villasmil@enreach.com>
2022-11-24 21:05:26 +0100

    working version

--- kamailio/start_kam.sh
+++ kamailio/start_kam.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 rm -rf /etc/kamailio
-git clone https://github.com/Viking-VoIP/full-voip-on-k8s-sip-proxy-config.git /etc/kamailio
+git clone  --progress --verbose https://github.com/Viking-VoIP/full-voip-on-k8s-sip-proxy-config.git /etc/kamailio
+echo "Cloned succesfully..."
+echo "Initializing now..."
 cd /etc/kamailio/ && bash initialize.sh
